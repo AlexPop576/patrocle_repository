371b1c2aa2d660f15500d85106f16635b7ab936c
AlexPop576 <apcode576@gmail.com>
2024-03-29 16:16:38 +0200

    database tables

--- lib/Database/database_helper.dart
+++ lib/Database/database_helper.dart
@@ -5,20 +5,14 @@ class DatabaseHelper {
   DatabaseHelper._internal();
   static DatabaseHelper get instance =>
       _databaseHelper ??= DatabaseHelper._internal();
-  Database? _db, _quizDB, _geographyCompletedDB, _historyCompletedDB, _trophies;
+  Database? _db,  _trophies;
   Database get db => _db!;
-  Database get quizDB => _quizDB!;
-  Database get geographyCompletedDB => _geographyCompletedDB!;
-  Database get historyCompletedDB => _historyCompletedDB!;
   Database get trophies => _trophies!;
   Future<void> init() async {
     _db =
         await openDatabase('database.db', version: 1, onCreate: (db, version) {
       db.execute(
           ' CREATE TABLE countries (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(255))');
-    });
-    _trophies =
-        await openDatabase('database_trophies.db', version: 1, onCreate: (trophies, version) {
       trophies.execute(
           ' CREATE TABLE trophies (id INTEGER PRIMARY KEY AUTOINCREMENT, index INT)');
     });
